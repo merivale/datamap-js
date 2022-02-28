bb11bdce6d970e2ddc5db9f4301742244b509638
Amyas Merivale <merivale@gmail.com>
2022-02-28 18:50:15 +0100

    Treat functions like other objects

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,16 +1,11 @@
 const stringify = (value: unknown): string => {
-  // callable objects
-  if (typeof value === 'function') {
-    return value.toString()
-  }
-
   // arrays
   if (Array.isArray(value)) {
     return `[${value.map(stringify).join()}]`
   }
 
   // other objects
-  if (typeof value === 'object' && value !== null) {
+  if (typeof value === 'function' || (typeof value === 'object' && value !== null)) {
     return `{${Object.entries(value).sort().map((key, value) => `"${key}":${stringify(value)}`).join()}}`
   }
 
